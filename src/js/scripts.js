f6d4fe1f423bf5be6de76ae2a3cd1e7dc9552788
Ashley Kolodziej <kolodz10@bu.edu>
2020-11-15 21:31:56 -0500

    Storing the data

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,5 @@
+import browserLocalstorage from 'browser-localstorage-expire';
+
 // Add your scripts here
 
 const container = document.querySelector( '#js-ideasResults' );
@@ -5,20 +7,40 @@ const container = document.querySelector( '#js-ideasResults' );
 // Goal: storing the API data for a certain amount of time
 // so I don't have to wait for the API call every single time
 
-fetch( 'https://api.raindrop.io/rest/v1/raindrops/13023541?perpage=50', {
-  headers: {
-		'Content-Type': 'application/json',
-		'Authorization': 'Bearer ' + process.env.OAUTH_TOKEN,
-	},
-} )
+const localCache = browserLocalstorage();
+let storedData = localCache.getItem("apiData");
+
+// Check if the data has been saved
+if ( storedData === null ) {
+  console.log("Fetching data");
+  getData();
+} else {
+  console.log("It was stored");
+  addMarkup( storedData );
+}
+
+function getData() {
+  fetch( 'https://api.raindrop.io/rest/v1/raindrops/13023541?perpage=50', {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + process.env.OAUTH_TOKEN,
+    },
+  } )
   .then( response => response.json() )
   .then( data => {
+    // Goal: Save the data
     console.log(data);
-    
-    for ( const item of data.items ) {
-      container.innerHTML += createIdeaMarkup(item);
-    }
+    storedData = data;
+    localCache.setItem("apiData", storedData);
+    addMarkup( storedData );
   } );
+}
+
+function addMarkup( data ) {
+  for ( const item of data.items ) {
+    container.innerHTML += createIdeaMarkup(item);
+  }
+}
 
 // Goal: showing TWO random bookmarks from my stored data
 // Do this each time the button is clicked
@@ -27,7 +49,7 @@ fetch( 'https://api.raindrop.io/rest/v1/raindrops/13023541?perpage=50', {
 
 // Goal: Return HTML with data from item
 function createIdeaMarkup( item ) {
-  
+
   // Goal: Create a template for my idea result
   const ideaTemplate = 
     `<article class="idea">
