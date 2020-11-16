64aa4805bc9af81dbcf1afe48452e7566430667b
Ashley Kolodziej <kolodz10@bu.edu>
2020-11-15 21:04:44 -0500

    Adds the general markup and item template

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,10 @@
 // Add your scripts here
 
+const container = document.querySelector( '#js-ideasResults' );
+
+// Goal: storing the API data for a certain amount of time
+// so I don't have to wait for the API call every single time
+
 fetch( 'https://api.raindrop.io/rest/v1/raindrops/13023541?perpage=50', {
   headers: {
 		'Content-Type': 'application/json',
@@ -11,6 +16,26 @@ fetch( 'https://api.raindrop.io/rest/v1/raindrops/13023541?perpage=50', {
     console.log(data);
     
     for ( const item of data.items ) {
-      console.log(item.title);
+      container.innerHTML += createIdeaMarkup(item);
     }
   } );
+
+// Goal: showing TWO random bookmarks from my stored data
+// Do this each time the button is clicked
+// Goal: Change button text to "Generate Again" after initial click
+// Goal: Add my results to js-ideasResults
+
+// Goal: Return HTML with data from item
+function createIdeaMarkup( item ) {
+  
+  // Goal: Create a template for my idea result
+  const ideaTemplate = 
+    `<article class="idea">
+      <img src="${item.media[0].link}" alt="Thumbnail for ${item.title}" class="idea-image" />
+      <h2 class="idea-title">${item.title}</h2>
+      <p class="idea-description">${item.excerpt}</p>
+      <a href="${item.link}" class="idea-learnmore">Learn more about ${item.title}</a>
+    </article>`;
+
+  return ideaTemplate;
+}
