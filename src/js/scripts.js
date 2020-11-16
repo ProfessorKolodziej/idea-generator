413c2c747a99b2709990caf5e9c69fc33f37f71a
Ashley Kolodziej <kolodz10@bu.edu>
2020-11-15 20:37:58 -0500

    Adds fetch call to raindrop.io api

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,16 @@
 // Add your scripts here
 
-console.log(process.env.OAUTH_TOKEN);
+fetch( 'https://api.raindrop.io/rest/v1/raindrops/13023541?perpage=50', {
+  headers: {
+		'Content-Type': 'application/json',
+		'Authorization': 'Bearer ' + process.env.OAUTH_TOKEN,
+	},
+} )
+  .then( response => response.json() )
+  .then( data => {
+    console.log(data);
+    
+    for ( const item of data.items ) {
+      console.log(item.title);
+    }
+  } );
